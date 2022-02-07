00912893686ba8cb071e93769d10c59ef5764aa7
Victor Petrosyan <viko20000@mail.ru>
2022-02-07 22:12:24 +0300

    The first step, after Anton's review 07 feb 2022

--- src/main/java/UrlValidator/Main.java
+++ src/main/java/UrlValidator/Main.java
@@ -10,11 +10,6 @@ public class Main {
         /*
         Это с последнего созвона пример от Антона:
 
-            Создаем класс UrlValidator передаю в него UrlValidator.Options(),
-            у которого устанавливаю authorityValidation, а внутри new DomainValidation,
-            который тоже с Options, где tldValidation стоит все считай true.
-            Вот такой конструктор получается. Могу гибко настраивать. Могу расширить,
-
             final UrlValidator validation = new UrlValidator(
                 new UrlValidator.Options().authorityValidation(
                     new DefaultAuthorityValidation(

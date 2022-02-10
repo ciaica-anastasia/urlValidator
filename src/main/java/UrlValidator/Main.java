423ede15d6c1a2b34a21d22f4f54412565e26e51
Anastasia326 <60912054+Anastasia326@users.noreply.github.com>
2022-02-10 12:27:19 +0300

    Update Main.java

--- src/main/java/UrlValidator/Main.java
+++ src/main/java/UrlValidator/Main.java
@@ -6,10 +6,14 @@ public class Main {
         UrlValidator validator = new Options().setNewLogins(logins -> false).build();
         UrlValidator test = new Options().setNewHosts(logins -> true).build();
         final UrlValidator tmp = new UrlValidator( new Options() );
-        System.out.println("OK!");
+        //Пользователь вводит свою url вместо "URL"
+        if (validator.isValid("URL")) {
+            System.out.println("url is valid");
+        } else {
+            System.out.println("url is invalid");
+        }
         /*
         Это с последнего созвона пример от Антона:
-
             final UrlValidator validation = new UrlValidator(
                 new UrlValidator.Options().authorityValidation(
                     new DefaultAuthorityValidation(
@@ -20,5 +24,6 @@ public class Main {
         */
 
         //  Пример плохого URL: http://game.my.srv:19100
+        // Еще один: https://стопкоронавирус.рф/?
     }
 }

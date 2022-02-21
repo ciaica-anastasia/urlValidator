a7ffd9f3c424e29e5f78b4830dd8a0fec14adbd1
Anastasia326 <60912054+Anastasia326@users.noreply.github.com>
2022-02-21 17:44:38 +0300

    Corrections of comments

--- src/main/java/UrlValidator/UrlParser.java
+++ src/main/java/UrlValidator/UrlParser.java
@@ -27,43 +27,53 @@ public final class UrlParser {
         }
         return Arrays.asList(url.substring(start, cut), url.substring(cut));
     }
+    
+    static class UrlSplitted {
+        private
+        String scheme;
+        String netloc;
+        String url;
+        String query;
+        String fragment;
+        public
+        UrlSplitted (String newScheme, String newNetloc, String newUrl, String newQuery, String newFragment) {
+            scheme = newScheme;
+            netloc = newNetloc;
+            url = newUrl;
+            query = newQuery;
+            fragment = newFragment;
+        }
+    }
+    
+    static class HostPort {
+        String hostname;
+        String port;
+        HostPort(String newHostname, String newPort) {
+            hostname = newHostname;
+            port = newPort;
+        }
+    }
 
-    private @NotNull List<String> urlSplit(@NotNull String url, @NotNull String scheme) {
+    private @NotNull UrlSplitted urlSplit(@NotNull String url, @NotNull String scheme) {
+        String newUrl = url;
+        String newScheme = scheme;
         for (String b : unsafeUrlBytesToRemove) {
-            url = url.replace(b, "");
-            scheme = scheme.replace(b, "");
+            newUrl = url.replace(b, "");
+            newScheme = newScheme.replace(b, "");
         }
         String netloc = "", query = "", fragment = "";
-        int i = url.indexOf(":");
-        char[] charUrl = url.toCharArray();
+        int i = newUrl.indexOf(":");
+        char[] charUrl = newUrl.toCharArray();
         if (i > 0) {
-            if (url.substring(0, i).equals("http")) {
-                scheme = "http";
-                url = url.substring(i + 1);
-                if (url.startsWith("//")) {
-                    netloc = splitNetcol(url, 2).get(0);
-                    url = splitNetcol(url, 2).get(1);
-                    //ТУТ ЕСТЬ ПРОВЕРКА На [, не знаю, нужна ли
-                }
-                if (url.contains("#")) {
-                    int indexCut = url.indexOf("#");
-                    String oldUrl = url;
-                    url = url.substring(0, indexCut);
-                    fragment = oldUrl.substring(indexCut + 1);
-                }
-                if (url.contains("?")) {
-                    int indexCut = url.indexOf("?");
-                    String oldUrl = url;
-                    url = url.substring(0, indexCut);
-                    query = oldUrl.substring(indexCut + 1);
-                }
-                // Тут может быть проверка netloc
-                return Arrays.asList(scheme, netloc, url, query, fragment);
+            if (newUrl.substring(0, i).equals("http")) {
+                newScheme = "http";
+                newUrl = newUrl.substring(i + 1);
+                return getUrlSplitted(newUrl, newScheme, netloc, query, fragment);
             }
             boolean flag = false;
             for (int j = 0; j < i; ++j) {
                 char c = charUrl[j];
-                if (!schemeChars.contains(Character.toString(c))) {
+                if (!scheme_chars.contains(Character.toString(c))) {
                     flag = true;
                     break;
                 }
@@ -71,7 +81,7 @@ public final class UrlParser {
             if (!flag) {
                 // make sure "url" is not actually a port number (in which case
                 // "scheme" is really part of the path)
-                char[] rest = url.substring(i + 1).toCharArray();
+                char[] rest = newUrl.substring(i + 1).toCharArray();
                 boolean flagAnother = false;
                 for (char c : rest) {
                     String numbers = "0123456789";
@@ -81,67 +91,59 @@ public final class UrlParser {
                     }
                 }
                 if (flagAnother) {
-                    scheme = url.substring(0, i);
-                    url = url.substring(i + 1);
+                    newScheme = newUrl.substring(0, i);
+                    newUrl = newUrl.substring(i + 1);
                 }
             }
         }
-        if (url.startsWith("//")) {
-            netloc = splitNetcol(url, 2).get(0);
-            url = splitNetcol(url, 2).get(1);
-            //ТУТ ЕСТЬ ПРОВЕРКА На [, не знаю, нужна ли
+        return getUrlSplitted(newUrl, newScheme, netloc, query, fragment);
+    }
+    
+    private @NotNull UrlParser.UrlSplitted getUrlSplitted(@NotNull String newUrl,@NotNull String newScheme, String netloc, String query, String fragment) {
+        if (newUrl.startsWith("//")) {
+            netloc = SplitNetcol(newUrl, 2).get(0);
+            newUrl = SplitNetcol(newUrl, 2).get(1);
         }
-        if (url.contains("#")) {
-            int indexCut = url.indexOf("#");
-            String oldUrl = url;
-            url = url.substring(0, indexCut);
+        if (newUrl.contains("#")) {
+            int indexCut = newUrl.indexOf('#');
+            String oldUrl = newUrl;
+            newUrl = newUrl.substring(0, indexCut);
             fragment = oldUrl.substring(indexCut + 1);
         }
-        if (url.contains("?")) {
-            int indexCut = url.indexOf("?");
-            String oldUrl = url;
-            url = url.substring(0, indexCut);
+        if (newUrl.contains("?")) {
+            int indexCut = newUrl.indexOf('?');
+            String oldUrl = newUrl;
+            newUrl = newUrl.substring(0, indexCut);
             query = oldUrl.substring(indexCut + 1);
         }
-        // Тут может быть проверка netloc
-        return Arrays.asList(scheme, netloc, url, query, fragment);
+        return new UrlSplitted(newScheme, netloc, newUrl, query, fragment);
     }
-
-    private @NotNull List<String> hostInfo(@NotNull String netloc) {
-        String hostname = netloc;
-        String port = "";
-        int index = netloc.indexOf("[");
+    
+    private @NotNull HostPort hostInfo(@NotNull String netloc) {
+        String hostname = netloc, port = "";
+        int index = netloc.indexOf('[');
         if (index != -1) {
-            String haveOpenBr = netloc.substring(0, index);
             String bracketEnd = netloc.substring(index + 1);
-            int indexEnd = bracketEnd.indexOf("]");
+            int indexEnd = bracketEnd.indexOf(']');
             hostname = bracketEnd.substring(0, indexEnd);
             port = bracketEnd.substring(indexEnd);
-            int indexPort = port.indexOf(":");
+            int indexPort = port.indexOf(':');
             port = port.substring(indexPort + 1);
         } else {
-            int indexPort = netloc.indexOf(":");
+            int indexPort = netloc.indexOf(':');
             if (indexPort != -1) {
                 hostname = netloc.substring(0, indexPort);
                 port = netloc.substring(indexPort + 1);
             }
         }
-        return Arrays.asList(hostname, port);
+        return new HostPort(hostname, port);
     }
 
     public @NotNull UrlElement parse(@NotNull String urlAddress) {
-        List<String> splitResult = urlSplit(urlAddress, "");
-
-        String scheme = splitResult.get(0);
-        String netloc = splitResult.get(1);
-        String url = splitResult.get(2);
-        String query = splitResult.get(3);
-        String fragment = splitResult.get(4);
+       UrlSplitted splitResult = UrlSplit(oldUrl, "");
 
         //Теперь надо отделить hostname и port
-        List<String> parsedHost = hostInfo(netloc);
-        String host = parsedHost.get(0);
-        String port = parsedHost.get(1);
-        return new UrlElement(host, scheme, port, query, url, fragment);
+        HostPort parsedHost = HostInfo(splitResult.netloc);
+        return new UrlElement(parsedHost.hostname, splitResult.scheme, parsedHost.port, splitResult.query, splitResult.url, splitResult.fragment);
     }
 }
